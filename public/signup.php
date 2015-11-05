b0c63a8739bd045f6ceac831c4f37ee5701830d8
itsshady101 <spamer420@yahoo.in>
2015-11-05 21:27:44 +0530

    Fourth Commit

--- public/signup.php
+++ public/signup.php
@@ -10,6 +10,15 @@
 
 	$nickname = $_POST["nickname"];
 	$username = $_POST["username"];
+
+	$query = "SELECT * from users where username = '{$username}' limit 1";
+	$result = mysqli_query($connection, $query);
+
+	if($result) {
+		$_SESSION["message"] = "Username not available.";
+		redirect_to("index.php");
+	}
+
 	$password = password_hash($_POST["password"], PASSWORD_BCRYPT);
 	$question = $_POST["question"];
 	$answer = password_hash($_POST["answer"], PASSWORD_BCRYPT);
